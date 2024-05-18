5a99156b691e280c02023216ea8dcc3c522a40d7
Nekoota <130983615+Nekoota@users.noreply.github.com>
2024-05-18 11:21:54 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config/config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// load command files dynamically
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// load event files dynamically
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.login(token);
